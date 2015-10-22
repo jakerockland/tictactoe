d601b383df63ab798e33f95b23f38f72fe4b7bf9
Jake Rockland <jakerockland@email.arizona.edu>
2015-10-22 16:02:03 +0200

    version 1.0 of learning implemented

--- tictactoe.py
+++ tictactoe.py
@@ -10,21 +10,22 @@
 # x2: number of instances of possibilities with 2 O's and an empty space
 # x3: number of instances of possibilities with 3 X's (game over)
 # x4: number of instances of possibilities with 3 O's (game over)
-# x5: number of empty squares
+# x5: number of instances of possibilities with 1 X and two empty spaces
+# x6: number of instances of possibilities with 1 O and two empty spaces
 
 import copy
 import random
 
 class ExperimentGenerator:
     def __init__(self):
-        self.board = self.newBoard()
+        self.board = self.newGame()
         self.history = [copy.deepcopy(self.board)]
 
-    def newBoard(self):
-    board = [ [' ',' ',' '],
-              [' ',' ',' '],
-              [' ',' ',' '] ]
-    return board
+    def newGame(self):
+        board = [ [' ',' ',' '],
+                  [' ',' ',' '],
+                  [' ',' ',' '] ]
+        return board
 
     def getBoard(self):
         return self.board
@@ -32,13 +33,6 @@ class ExperimentGenerator:
     def setBoard(self,board):
         self.board = board
         self.history.append(copy.deepcopy(self.board))
-
-    def printBoard(self,board = None):
-        if board is None:
-            board = self.board
-        print('\n' + board[0][0] + '|' + board[0][1] + '|' + board[0][2])
-        print('-----' + board[1][0] + '|' + board[1][1] + '|' + board[1][2])
-        print('-----' + board[2][0] + '|' + board[2][1] + '|' + board[2][2] + '\n')
 
     def getHistory(self):
         return self.history
@@ -52,6 +46,30 @@ class ExperimentGenerator:
                 if square == ' ':
                     num += 1
         return num
+
+    def getPossibilities(self, board = None):
+        if board is None:
+            board = self.board
+        possibilities = []
+        for row in board:
+            possibilities.append(row)
+        for x in range(3):
+            column = []
+            column.append(board[0][x])
+            column.append(board[1][x])
+            column.append(board[2][x])
+            possibilities.append(column)
+        diagonal_1 = []
+        diagonal_1.append(board[2][0])
+        diagonal_1.append(board[1][1])
+        diagonal_1.append(board[0][2])
+        possibilities.append(diagonal_1)
+        diagonal_2 = []
+        diagonal_2.append(board[0][0])
+        diagonal_2.append(board[1][1])
+        diagonal_2.append(board[2][2])
+        possibilities.append(diagonal_2)
+        return possibilities
 
     def getWinner(self, board = None):
         if board is None:
@@ -71,15 +89,80 @@ class ExperimentGenerator:
                 return 'O'
         return None
 
-    def getFeatures(self, board = None):
+    def makeMove(self,x,y,token):
+        if (y in range(3)) and (x in range(3)) and (self.board[y][x] == ' '):
+            self.board[y][x] = token
+            self.history.append(copy.deepcopy(self.board))
+            return True
+        else:
+            return False
+
+    def getXSuccessors(self):
+        successors = []
+        for i in range(3):
+            for j in range(3):
+                if self.board[i][j] == ' ':
+                    successor = copy.deepcopy(self.board)
+                    successor[i][j] = 'X'
+                    successors.append(successor)
+        return successors
+
+    def getOSuccessors(self):
+        successors = []
+        for i in range(3):
+            for j in range(3):
+                if self.board[i][j] == ' ':
+                    successor = copy.deepcopy(self.board)
+                    successor[i][j] = 'O'
+                    successors.append(successor)
+        return successors
+
+    def printBoard(self,board = None):
         if board is None:
             board = self.board
-        possibilities = self.getPossibilities()
+        print('\n' + board[0][0] + '|' + board[0][1] + '|' + board[0][2])
+        print('-----\n' + board[1][0] + '|' + board[1][1] + '|' + board[1][2])
+        print('-----\n' + board[2][0] + '|' + board[2][1] + '|' + board[2][2] + '\n')
+
+
+class PerformanceSystem:
+    def __init__(self,game,hypothesis,mode = 'X'):
+        self.game = game
+        self.hypothesis = hypothesis
+        self.mode = mode
+        self.update_constant = 0.1
+
+    def setUpdateConstant(self, constant):
+        self.update_constant = update_constant
+
+    def getGame(self):
+        return self.game
+
+    def setGame(self,game):
+        self.game = game
+
+    def getMode(self):
+        return self.mode
+
+    def setMode(self,mode):
+        self.mode = mode
+
+    def getHypothesis(self):
+        return self.hypothesis
+
+    def setHypothesis(self, hypothesis):
+        self.hypothesis = hypothesis
+
+    def getFeatures(self, board = None):
+        if board is None:
+            board = self.game.getBoard()
+        possibilities = self.game.getPossibilities()
         x1 = 0
         x2 = 0
         x3 = 0
         x4 = 0
-        x5 = self.numEmptySquares()
+        x5 = 0
+        x6 = 0
         for possibility in possibilities:
             X_count = 0
             O_count = 0
@@ -96,53 +179,177 @@ class ExperimentGenerator:
                 x3 += 1
             elif O_count == 3:
                 x4 += 1
-        return x1,x2,x3,x4,x5
+            elif X_count == 1 and O_count == 0:
+                x5 += 1
+            elif O_count == 1 and X_count == 0:
+                x6 += 1
+        return x1,x2,x3,x4,x5,x6
 
-    def getPossibilities(self, board = None):
+    def chooseSmart(self):
+        if self.mode == 'X':
+            successors = self.game.getXSuccessors()
+        else:
+            successors = self.game.getOSuccessors()
+        best_successor = successors[0]
+        best_value = self.performEvaluation(best_successor)
+        for successor in successors:
+            value = self.performEvaluation(successor)
+            if value > best_value:
+                best_value = value
+                best_successor = successor
+        self.game.setBoard(best_successor)
+
+    def performEvaluation(self, board = None):
         if board is None:
-            board = self.board
-        possibilities = []
-        for row in board:
-            possibilities.append(row)
-        for x in range(0,3):
-            column = []
-            column.append(board[0][x])
-            column.append(board[1][x])
-            column.append(board[2][x])
-            possibilities.append(column)
-        diagonal_1 = []
-        diagonal_1.append(board[2][0])
-        diagonal_1.append(board[1][1])
-        diagonal_1.append(board[0][2])
-        possibilities.append(diagonal_1)
-        diagonal_2 = []
-        diagonal_2.append(board[0][0])
-        diagonal_2.append(board[1][1])
-        diagonal_2.append(board[2][2])
-        possibilities.append(diagonal_2)
-        return possibilities
+            board = self.game.getBoard()
+        x1,x2,x3,x4,x5,x6 = self.getFeatures(board)
+        w0,w1,w2,w3,w4,w5,x6 = self.hypothesis
+        return w0 + w1*x1 + w2*x2 + w3*x3 + w4*x4 + w5*x5 + w6*x6
 
-    def getXSuccessors(self):
-        successors = []
-        for i in range(0,3):
-            for j in range(0,3):
-                if self.board[i][j] == ' ':
-                    successor = copy.deepcopy(self.board)
-                    successor[i][j] = 'X'
-                    successors.append(successor)
-        return successors
 
-    def getOSuccessors(self):
-        successors = []
-        for i in range(0,3):
-            for j in range(0,3):
-                if self.board[i][j] == ' ':
-                    successor = copy.deepcopy(self.board)
-                    successor[i][j] = 'O'
-                    successors.append(successor)
-        return successors
+class Critic:
+        def __init__(self,performance_system):
+        self.performance_system = performance_system
+        self.game = performance_system.getGame()
 
-    def makeMove(self,x,y,token,add_history = False):
-        self.board[y][x] = token
-        if add_history:
-            self.history.append(copy.deepcopy(self.board))
+    def getTrainingExamples(self):
+        history = self.game.getHistory()
+        training_examples = []
+        for i in range(len(history)):
+            mode = self.performance_system.getMode()
+            winner = self.game.getWinner(history[i])
+            empty_squares = self.game.numEmptySquares(history[i])
+            board_features = self.game.getFeatures(history[i])
+            if winner == mode:
+                training_examples.append([board_features,100])
+            elif winner is None:
+                if empty_squares == 0:
+                    training_examples.append([board_features,0])
+                else:
+                    if i+2 >= len(history):
+                        if self.game.getWinner(history[len(history)-1]) == 0:
+                            training_examples.append([board_features,0])
+                        else:
+                            training_examples.append([board_features,-100])
+                    else:
+                        training_examples.append(board_features,self.performance_system.performEvaluation(history[i+2])])
+            else:
+                training_examples.append([board_features,-100])
+        return training_examples
+
+
+class Generalizer:
+    def __init__(self,hypothesis):
+        self.hypothesis = hypothesis
+
+    def updateHypothesis(self,history,training_examples):
+        for i in range(len(history)):
+            w0,w1,w2,w3,w4,w5,w6 = self.hypothesis
+            v_eval = self.performEvaluation(history[i])
+            x1,x2,x3,x4,x5,x6 = training_examples[i][0]
+            v_train = training_examples[i][1]
+            w0 = w0 + self.update_constant*(v_train - v_eval)
+            w1 = w1 + self.update_constant*(v_train - v_eval)*x1
+            w2 = w2 + self.update_constant*(v_train - v_eval)*x2
+            w3 = w3 + self.update_constant*(v_train - v_eval)*x3
+            w4 = w4 + self.update_constant*(v_train - v_eval)*x4
+            w5 = w5 + self.update_constant*(v_train - v_eval)*x5
+            w6 = w6 + self.update_constant*(v_train - v_eval)*x6
+            self.hypothesis = w0,w1,w2,w3,w4,w5,w6
+            return self.hypothesis
+
+
+class Trainer:
+    def __init__(self,game,mode = 'O'):
+        self.game = game
+        self.mode = mode
+
+    def getGame(self):
+        return self.game
+
+    def setGame(self,game):
+        self.game = game
+
+    def getMode(self):
+        return self.mode
+
+    def setMode(self,mode):
+        self.mode = mode
+
+    def chooseRandom(self):
+        if self.mode == 'X':
+            successors = self.game.getXSuccessors()
+        else:
+            successors = self.game.getOSuccessors()
+        self.game.setBoard(successors[random.randint(0,len(successors)-1)])
+
+    def chooseTrain(self):
+        # FIXME
+        return
+
+
+def train():
+    game = ExperimentGenerator()
+    hypothesis = (0.5,0.5,0.5,0.5,0.5,0.5)
+    learner = PerformanceSystem(game,hypothesis,'X')
+    trainer = Trainer(game,'O')
+    generalizer = Generalizer(hypothesis)
+
+    learner_wins = 0
+    trainer_wins = 0
+    total_games = 0
+
+    start_turn = random.randint(0,1)]
+    if start_turn == 1:
+        player_turn == 'X'
+    else:
+        player_turn == 'O'
+
+    for i in range(1000):
+        game = ExperimentGenerator()
+        learner.setGame(game)
+        trainer.setGame(game)
+        while (game.getWinner is None) and (game.numEmptySquares > 0):
+            if player_turn == 'X':
+                learner.chooseMove()
+                player_turn == 'O'
+            else:
+                trainer.chooseRandom()
+                player_turn == 'X'
+
+        winner = game.getWinner()
+        if winner == 'X':
+            learner_wins += 1
+        elif winner == 'O':
+            trainer_wins += 1
+        total_games += 1
+        print("Games Played: " + str(total_games))
+        print("Percentage Won: " + str((learner_wins / total_games) * 100) + "\n")
+
+        critic = Critic(learner)
+        training_examples = critic.getTrainingExamples()
+        learner.setHypothesis = generalizer.updateHypothesis(game.getHistory(),training_examples)
+
+
+def main():
+    train()
+    # game = ExperimentGenerator()
+    # game.printBoard()
+    # while game.numEmptySquares() > 0:
+    #     if game.numEmptySquares() % 2 == 0:
+    #         print("Player X's Turn")
+    #     else:
+    #         print("Player O's Turn")
+    #     x = int(input("Enter x coordinate [0,2]: "))
+    #     y = int(input("Enter y coordinate: [0,2]: "))
+    #     if game.numEmptySquares() % 2 == 0:
+    #         legal_move = game.makeMove(x,y,'X')
+    #     else:
+    #         legal_move = game.makeMove(x,y,'O')
+    #     if not legal_move:
+    #         print("Illegal move.")
+    #     game.printBoard()
+    # for board in game.getHistory():
+    #     game.printBoard(board)
+
+main()
